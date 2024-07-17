12592d3856e126e9229a6b8f99bd9de0b857d01c
Daurius Swarts <ali@openmindi.co.za>
2024-07-17 15:07:06 +0200

    Add audioo files

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,23 +16,90 @@ const playBtn = document.querySelector(".play"),
 
 this.tracks = [
     {
-        name: "Nama",
-        artist: "Artist 1",
-        cover: "https://images.unsplash.com/photo-1494232410401-ad00d5433cfa?crop=entropy&cs=tinysrgb&fm=jpg",
+        name: "Track 1 - Clicks",
+        artist: "Nama Language",
+        cover: "assets/nama.png",
+        source: "assets/audio/track1.mp3",
+    },
+    {
+        name: "Track 2 - Vowels",
+        artist: "Nama Language",
+        cover: "assets/nama.png",
+        source: "assets/audio/track2.mp3",
+    },
+    {
+        name: "Track 3 - Alphabet",
+        artist: "Nama Language",
+        cover: "assets/nama.png",
+        source: "assets/audio/track3.mp3",
+    },
+    {
+        name: "Track 4 - Nouns",
+        artist: "Nama Language",
+        cover: "assets/nama.png",
+        source: "assets/audio/track4.mp3",
+    },
+    {
+        name: "Track 5 - Glossary",
+        artist: "Nama Language",
+        cover: "assets/nama.png",
+        source: "assets/audio/track5.mp3",
+    },
+    {
+        name: "Track 6 - Foreign words",
+        artist: "Nama Language",
+        cover: "assets/nama.png",
+        source: "assets/audio/track6.mp3",
+    },
+    {
+        name: "Track 7 - Greetings",
+        artist: "Nama Language",
+        cover: "assets/nama.png",
+        source: "assets/audio/track7.mp3",
+    },
+    {
+        name: "Track 8 - Weather",
+        artist: "Nama Language",
+        cover: "assets/nama.png",
+        source: "assets/audio/track8.mp3",
+    },
+    {
+        name: "Track 9 - Seasons",
+        artist: "Nama Language",
+        cover: "assets/nama.png",
+        source: "assets/audio/track9.mp3",
+    },
+    {
+        name: "Track 10 - Planets",
+        artist: "Nama Language",
+        cover: "assets/nama.png",
+        source: "assets/audio/track10.mp3",
+    },
+    {
+        name: "Track 11 - Numbers",
+        artist: "Nama Language",
+        cover: "assets/nama.png",
+        source: "assets/audio/track11.mp3",
+    },
+    {
+        name: "Track 12 - Ordinals",
+        artist: "Nama Language",
+        cover: "assets/nama.png",
+        source: "assets/audio/track12.mp3",
+    },
+    {
+        name: "Track 13 - Essensielle",
+        artist: "Nama Language",
+        cover: "assets/nama.png",
         source: "assets/audio/track13.mp3",
     },
     {
-        name: "Hip Hop 02",
-        artist: "Artist 2",
-        cover: "https://images.unsplash.com/photo-1485579149621-3123dd979885?crop=entropy&cs=tinysrgb&fm=jpg",
-        source: "https://assets.mixkit.co/music/download/mixkit-hip-hop-02-738.mp3",
+        name: "Track 14 - Classroom",
+        artist: "Nama Language",
+        cover: "assets/nama.png",
+        source: "assets/audio/track14.mp3",
     },
-    {
-        name: "Dreaming Big",
-        artist: "Artist 3",
-        cover: "https://images.unsplash.com/photo-1516280440614-37939bbacd81?crop=entropy&cs=tinysrgb&fm=jpg",
-        source: "https://assets.mixkit.co/music/download/mixkit-dreaming-big-31.mp3",
-    },
+
 ];
 
 // Initial state values
