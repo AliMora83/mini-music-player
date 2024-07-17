79a7b82c90b2444568f70c6681f4dd6350328e88
Daurius Swarts <ali@openmindi.co.za>
2024-07-17 23:42:52 +0200

    Added tracks file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+import { tracks } from './tracks.js';
+
 const playBtn = document.querySelector(".play"),
     skipForward = document.querySelector(".skip-forward"),
     skipBack = document.querySelector(".skip-back"),
@@ -13,94 +15,6 @@ const playBtn = document.querySelector(".play"),
     img = document.querySelector('.img'),
     title = document.querySelector(".audio-title"),
     singer = document.querySelector(".audio-singer");
-
-this.tracks = [
-    {
-        name: "Track 1 - Clicks",
-        artist: "Nama Language",
-        cover: "assets/nama.png",
-        source: "assets/audio/track1.mp3",
-    },
-    {
-        name: "Track 2 - Vowels",
-        artist: "Nama Language",
-        cover: "assets/nama.png",
-        source: "assets/audio/track2.mp3",
-    },
-    {
-        name: "Track 3 - Alphabet",
-        artist: "Nama Language",
-        cover: "assets/nama.png",
-        source: "assets/audio/track3.mp3",
-    },
-    {
-        name: "Track 4 - Nouns",
-        artist: "Nama Language",
-        cover: "assets/nama.png",
-        source: "assets/audio/track4.mp3",
-    },
-    {
-        name: "Track 5 - Glossary",
-        artist: "Nama Language",
-        cover: "assets/nama.png",
-        source: "assets/audio/track5.mp3",
-    },
-    {
-        name: "Track 6 - Foreign words",
-        artist: "Nama Language",
-        cover: "assets/nama.png",
-        source: "assets/audio/track6.mp3",
-    },
-    {
-        name: "Track 7 - Greetings",
-        artist: "Nama Language",
-        cover: "assets/nama.png",
-        source: "assets/audio/track7.mp3",
-    },
-    {
-        name: "Track 8 - Weather",
-        artist: "Nama Language",
-        cover: "assets/nama.png",
-        source: "assets/audio/track8.mp3",
-    },
-    {
-        name: "Track 9 - Seasons",
-        artist: "Nama Language",
-        cover: "assets/nama.png",
-        source: "assets/audio/track9.mp3",
-    },
-    {
-        name: "Track 10 - Planets",
-        artist: "Nama Language",
-        cover: "assets/nama.png",
-        source: "assets/audio/track10.mp3",
-    },
-    {
-        name: "Track 11 - Numbers",
-        artist: "Nama Language",
-        cover: "assets/nama.png",
-        source: "assets/audio/track11.mp3",
-    },
-    {
-        name: "Track 12 - Ordinals",
-        artist: "Nama Language",
-        cover: "assets/nama.png",
-        source: "assets/audio/track12.mp3",
-    },
-    {
-        name: "Track 13 - Essensielle",
-        artist: "Nama Language",
-        cover: "assets/nama.png",
-        source: "assets/audio/track13.mp3",
-    },
-    {
-        name: "Track 14 - Classroom",
-        artist: "Nama Language",
-        cover: "assets/nama.png",
-        source: "assets/audio/track14.mp3",
-    },
-
-];
 
 // Initial state values
 let audio = null,
@@ -180,7 +94,7 @@ skipBack.addEventListener('click', () => {
     if (currentTrackIndex > 0) {
         currentTrackIndex--;
     } else {
-        this.currentTrackIndex = this.tracks.length - 1;
+        currentTrackIndex = tracks.length - 1;
     }
     currentTrack = tracks[currentTrackIndex];
 
---
+++ assets/js/tracks.js
@@ -0,0 +1,86 @@
+export const tracks = [
+    {
+        "name": "Track 1 - Clicks",
+        "artist": "Nama Language",
+        "cover": "assets/nama.png",
+        "source": "assets/audio/track1.mp3"
+    },
+    {
+        "name": "Track 2 - Vowels",
+        "artist": "Nama Language",
+        "cover": "assets/nama.png",
+        "source": "assets/audio/track2.mp3"
+    },
+    {
+        "name": "Track 3 - Alphabet",
+        "artist": "Nama Language",
+        "cover": "assets/nama.png",
+        "source": "assets/audio/track3.mp3"
+    },
+    {
+        "name": "Track 4 - Nouns",
+        "artist": "Nama Language",
+        "cover": "assets/nama.png",
+        "source": "assets/audio/track4.mp3"
+    },
+    {
+        "name": "Track 5 - Glossary",
+        "artist": "Nama Language",
+        "cover": "assets/nama.png",
+        "source": "assets/audio/track5.mp3"
+    },
+    {
+        "name": "Track 6 - Foreign words",
+        "artist": "Nama Language",
+        "cover": "assets/nama.png",
+        "source": "assets/audio/track6.mp3"
+    },
+    {
+        "name": "Track 7 - Greetings",
+        "artist": "Nama Language",
+        "cover": "assets/nama.png",
+        "source": "assets/audio/track7.mp3"
+    },
+    {
+        "name": "Track 8 - Weather",
+        "artist": "Nama Language",
+        "cover": "assets/nama.png",
+        "source": "assets/audio/track8.mp3"
+    },
+    {
+        "name": "Track 9 - Seasons",
+        "artist": "Nama Language",
+        "cover": "assets/nama.png",
+        "source": "assets/audio/track9.mp3"
+    },
+    {
+        "name": "Track 10 - Planets",
+        "artist": "Nama Language",
+        "cover": "assets/nama.png",
+        "source": "assets/audio/track10.mp3"
+    },
+    {
+        "name": "Track 11 - Numbers",
+        "artist": "Nama Language",
+        "cover": "assets/nama.png",
+        "source": "assets/audio/track11.mp3"
+    },
+    {
+        "name": "Track 12 - Ordinals",
+        "artist": "Nama Language",
+        "cover": "assets/nama.png",
+        "source": "assets/audio/track12.mp3"
+    },
+    {
+        "name": "Track 13 - Essensielle",
+        "artist": "Nama Language",
+        "cover": "assets/nama.png",
+        "source": "assets/audio/track13.mp3"
+    },
+    {
+        "name": "Track 14 - Classroom",
+        "artist": "Nama Language",
+        "cover": "assets/nama.png",
+        "source": "assets/audio/track14.mp3"
+    }
+]
